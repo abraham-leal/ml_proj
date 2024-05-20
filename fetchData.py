78e2ef52b6252e58ad80e3acbedff1de0c8e8fa3
Abraham Leal <aleal@confluent.io>
2024-05-20 12:49:37 -0500

    initial commit

---
+++ fetchData.py
@@ -0,0 +1,88 @@
+import datasets
+import ray.data
+import torchtext
+import wandb
+import torch
+import torch.nn as nn
+
+from NBoW import NBoW
+
+special_tokens = ["<unk>", "<pad>"]
+
+def tokenize(example, tokenizer, max_length):
+    tokens = tokenizer(example["text"])[:max_length]
+    return {"tokens": tokens}
+
+def numericalize(example, vocab):
+    ids = vocab.lookup_indices(example["tokens"])
+    return {"ids": ids}
+
+def get_collate_fn(pad_index):
+    def collate_fn(batch):
+        batch_ids = [i["ids"] for i in batch]
+        batch_ids = nn.utils.rnn.pad_sequence(
+            batch_ids, padding_value=pad_index, batch_first=True
+        )
+        batch_label = [i["label"] for i in batch]
+        batch_label = torch.stack(batch_label)
+        batch = {"ids": batch_ids, "label": batch_label}
+        return batch
+
+    return collate_fn
+
+def get_data_loader(dataset, batch_size, pad_index, shuffle=False):
+    collate_fn = get_collate_fn(pad_index)
+    data_loader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        collate_fn=collate_fn,
+        shuffle=shuffle,
+    )
+    return data_loader
+
+def fetchAndTokenize():
+    # Get data from hugging face and import into ray's datasets
+    train_data, test_data = datasets.load_dataset("imdb", split=["train", "test"])
+    tokenizer = torchtext.data.utils.get_tokenizer("basic_english")
+
+    tokenized_train_data = train_data.map(
+        tokenize, fn_kwargs={"tokenizer": tokenizer, "max_length": wandb.config["max_length"]}
+    )
+    tokenized_test_data = test_data.map(
+        tokenize, fn_kwargs={"tokenizer": tokenizer, "max_length": wandb.config["max_length"]}
+    )
+
+    tokenized_train_valid_data = tokenized_train_data.train_test_split(test_size=wandb.config["test_size"])
+    tokenized_valid_train_data = tokenized_train_valid_data["train"]
+    tokenized_valid_valid_data = tokenized_train_valid_data["test"]
+
+    vocab = torchtext.vocab.build_vocab_from_iterator(
+        tokenized_train_valid_data["train"]["tokens"],
+        min_freq=wandb.config["min_freq"],
+        specials=special_tokens,
+    )
+
+    unk_index = vocab["<unk>"]
+    pad_index = vocab["<pad>"]
+
+    vocab.set_default_index(unk_index)
+
+    numericalized_train_data = tokenized_valid_train_data.map(numericalize, fn_kwargs={"vocab": vocab})
+    numericalized_valid_data = tokenized_valid_valid_data.map(numericalize, fn_kwargs={"vocab": vocab})
+    numericalized_test_data = tokenized_test_data.map(numericalize, fn_kwargs={"vocab": vocab})
+
+    torch_train_data = numericalized_train_data.with_format(type="torch", columns=["ids", "label"])
+    torch_valid_data = numericalized_valid_data.with_format(type="torch", columns=["ids", "label"])
+    torch_test_data = numericalized_test_data.with_format(type="torch", columns=["ids", "label"])
+
+    train_data_loader = get_data_loader(torch_train_data, wandb.config["batch_size"], pad_index, shuffle=True)
+    valid_data_loader = get_data_loader(torch_valid_data, wandb.config["batch_size"], pad_index)
+    test_data_loader = get_data_loader(torch_test_data, wandb.config["batch_size"], pad_index)
+
+    output_dim = len(torch_train_data.unique("label"))
+
+    model = NBoW(len(vocab), wandb.config["embedding_dim"], output_dim, pad_index)
+
+    return vocab, model, train_data_loader, valid_data_loader, test_data_loader
+
+
---
+++ pywords.py
@@ -0,0 +1,115 @@
+import collections
+
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchtext
+import tqdm
+import wandb
+from fetchData import fetchAndTokenize
+
+run = wandb.init(project="bagofwords-test")
+wandb.config = {"epochs": 100, "batch_size": 512,
+                "seed": 1234, "max_length": 256,
+                "test_size": 0.25, "min_freq": 5,
+                "embedding_dim": 300}
+
+np.random.seed(wandb.config["seed"])
+torch.manual_seed(wandb.config["seed"])
+torch.cuda.manual_seed(wandb.config["seed"])
+torch.backends.cudnn.deterministic = True
+
+
+def train(data_loader, model, criterion, optimizer, device):
+    model.train()
+    epoch_losses = []
+    epoch_accs = []
+    for batch in tqdm.tqdm(data_loader, desc="training..."):
+        ids = batch["ids"].to(device)
+        label = batch["label"].to(device)
+        prediction = model(ids)
+        loss = criterion(prediction, label)
+        accuracy = get_accuracy(prediction, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch_losses.append(loss.item())
+        epoch_accs.append(accuracy.item())
+    return np.mean(epoch_losses), np.mean(epoch_accs)
+
+
+def evaluate(data_loader, model, criterion, device):
+    model.eval()
+    epoch_losses = []
+    epoch_accs = []
+    with torch.no_grad():
+        for batch in tqdm.tqdm(data_loader, desc="evaluating..."):
+            ids = batch["ids"].to(device)
+            label = batch["label"].to(device)
+            prediction = model(ids)
+            loss = criterion(prediction, label)
+            accuracy = get_accuracy(prediction, label)
+            epoch_losses.append(loss.item())
+            epoch_accs.append(accuracy.item())
+    return np.mean(epoch_losses), np.mean(epoch_accs)
+
+
+def get_accuracy(prediction, label):
+    batch_size, _ = prediction.shape
+    predicted_classes = prediction.argmax(dim=-1)
+    correct_predictions = predicted_classes.eq(label).sum()
+    accuracy = correct_predictions / batch_size
+    return accuracy
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def predict_sentiment(text, model, tokenizer, vocab, device):
+    tokens = tokenizer(text)
+    ids = vocab.lookup_indices(tokens)
+    tensor = torch.LongTensor(ids).unsqueeze(dim=0).to(device)
+    prediction = model(tensor).squeeze(dim=0)
+    probability = torch.softmax(prediction, dim=-1)
+    predicted_class = prediction.argmax(dim=-1).item()
+    predicted_probability = probability[predicted_class].item()
+    return predicted_class, predicted_probability
+
+
+vocab, model, train_data_loader, valid_data_loader, test_data_loader = fetchAndTokenize()
+
+print(f"The model has {count_parameters(model):,} trainable parameters")
+
+vectors = torchtext.vocab.GloVe()
+pretrained_embedding = vectors.get_vecs_by_tokens(vocab.get_itos())
+model.embedding.weight.data = pretrained_embedding
+optimizer = optim.Adam(model.parameters())
+criterion = nn.CrossEntropyLoss()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+criterion = criterion.to(device)
+best_valid_loss = float("inf")
+metrics = collections.defaultdict(list)
+
+# Magic
+wandb.watch(model, log_freq=100)
+
+for epoch in range(wandb.config["epochs"]):
+    train_loss, train_acc = train(
+        train_data_loader, model, criterion, optimizer, device
+    )
+    valid_loss, valid_acc = evaluate(valid_data_loader, model, criterion, device)
+    metrics["train_losses"].append(train_loss)
+    metrics["train_accs"].append(train_acc)
+    metrics["valid_losses"].append(valid_loss)
+    metrics["valid_accs"].append(valid_acc)
+    wandb.log({"accuracy": train_acc, "loss": train_loss, "valid_loss": valid_loss, "valid_acc": valid_acc})
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), "nbow.pt")
+    print(f"epoch: {epoch}")
+    print(f"train_loss: {train_loss:.3f}, train_acc: {train_acc:.3f}")
+    print(f"valid_loss: {valid_loss:.3f}, valid_acc: {valid_acc:.3f}")
+
